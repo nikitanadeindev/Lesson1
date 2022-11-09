3563c78861eba7b98bbfc42e2018ae9f7ff61a23
c1aymore <nikitanadeindev@gmail.com>
2022-11-09 20:17:50 +0300

    Add new random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,3 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
+double numberB = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
 Console.WriteLine(numberA / numberB);
